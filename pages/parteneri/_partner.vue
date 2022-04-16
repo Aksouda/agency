c163f17bd2ae83266e720186494d4fa92b94ec40
Aksouda <76596327+Aksouda@users.noreply.github.com>
2022-04-17 01:34:23 +0300

    png static

--- pages/parteneri/_partner.vue
+++ pages/parteneri/_partner.vue
@@ -48,7 +48,7 @@
           <img
             class="responsive"
             alt="Comparingpeople"
-            :src="require('@/assets/'+partner+'.png')"
+            :src="require('@/assets/ethicalestate.png')"
             loading="lazy"
           >
         </v-col>

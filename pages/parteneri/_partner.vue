8f4b578cb55afd2f4786fbf81d776d66cba1bcac
Aksouda <76596327+Aksouda@users.noreply.github.com>
2022-04-17 03:05:28 +0300

    inside psna

--- pages/parteneri/_partner.vue
+++ pages/parteneri/_partner.vue
@@ -15,7 +15,7 @@
             {{ $t('frontpage.description.top_candidate_banner_hdpi') }}
           </h1>
           <br>
-          <p>{{ $t('frontpage.description.info_hdpi') }}<span v-html="partner"></span><span v-html="$t('frontpage.description.info_hdpi1"></span>
+          <p>{{ $t('frontpage.description.info_hdpi') }}<span v-html="partner"></span><span>{{ $t('frontpage.description.info_hdpi1') }}</span>
           <p/>
           <v-btn
             x-large

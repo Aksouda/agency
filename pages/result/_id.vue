0b9b43dc5687102c5e2062b357a77ac432ac3475
Aksouda <76596327+Aksouda@users.noreply.github.com>
2022-08-26 18:13:07 +0300

    testlink

--- pages/result/_id.vue
+++ pages/result/_id.vue
@@ -71,7 +71,7 @@ var testid = window.location.href;
 console.log(Openness);
 const dealId1 = localStorage.getItem('dealId');
 console.log(dealId1);
-let bitrixapi = 'https://bitrix.monefy.ro/rest/52/kaals3ixqjdtjz7e/crm.deal.update.json?id='+dealId1+'&fields[UF_CRM_1647528341000]='+Extraversion+'&fields[UF_CRM_1647528370287]='+Openness+'&fields[UF_CRM_1647528380638]='+Conscientiousness+'&fields[UF_CRM_1647528390894]='+Agreeableness+'&fields[UF_CRM_1647528403430]='+Neuroticism+'&fields[STAGE_ID]=C3:FINAL_INVOICE';
+let bitrixapi = 'https://bitrix.monefy.ro/rest/52/kaals3ixqjdtjz7e/crm.deal.update.json?id='+dealId1+'&fields[UF_CRM_1647528341000]='+Extraversion+'&fields[UF_CRM_1647528370287]='+Openness+'&fields[UF_CRM_1647528380638]='+Conscientiousness+'&fields[UF_CRM_1647528390894]='+Agreeableness+'&fields[UF_CRM_1647528403430]='+Neuroticism+'&fields[UF_CRM_1647528425110]='+testid+'&fields[STAGE_ID]=C3:FINAL_INVOICE';
 fetch(bitrixapi, {
   method: 'POST'
 })
